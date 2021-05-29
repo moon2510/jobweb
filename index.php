d6e544210ed188ab5a181e473f95522b0c06dcb3
moon2510 <62028865+moon2510@users.noreply.github.com>
2021-05-29 20:35:38 +0700

    v1

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="/CSS/layout.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+   <link href="//db.onlinewebfonts.com/c/cefd416d4a810c38e8e2987afa6ef07f?family=Grendel's+Mother" rel="stylesheet"
+      type="text/css" />
+   <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+   <title>3D Job Web </title>
+</head>
+
+<body>
+   <div class="pageone">
+      <div class="info">
+         <div class="left">
+            <div class="logo">
+               <h6>Remotely</h6>
+            </div>
+         </div>
+         <div class="intro">
+            <div class="getnew">
+               <h1>Get New <i class="fas fa-cube"></i> Opportunity </h1>
+               <h3>But I must explain to you how all this mistaken
+                  idea of denouncing pleasure and praising pain.</h3>
+               <div class="getmail">
+                  <form action=""><input type="text" placeholder="Type email address"><button>Get Started</button></form>
+               </div>
+               <div class="usedby">
+                  <h4>Used by:</h4>
+                  <div class="icon"><i class="fab fa-paypal"></i> PayPal</div>
+                  <div class="icon"><i class="fab fa-dropbox"></i> Dropbox</div>
+                  <div class="icon"><i class="fab fa-google"></i> Google</div>
+                  <div class="icon"><i class="fab fa-apple"></i> Apple</div>
+               </div>
+            </div>
+            <div class="arrow">
+               <img src="/Image/arrow.png" alt="">
+            </div>
+         </div>
+      </div>
+      <div class="manimage">
+         <div class="right">
+            <div class="navbar">
+               <button class="snip">Home</button>
+               <button class="snip">Career</button>
+               <button class="snip">Projects</button>
+               <button class="snip">Contact</button>
+         
+            </div>
+            <div class="sign">
+               <button class="signin">Sign In</button>
+            </div>
+         </div>
+         <div class="manbook">
+            <img src="/Image/7bg.png" alt="">
+         </div>
+      </div>
+      
+   </div>
+   <div class="pagetwo">
+      <div class="boxwhitecenter">
+         <div class="boxwhite">
+            <h4 id="hiring">Hiring? Candidates are looking for you.</h4>
+            <div class="data">
+               <div class="numdata">
+                  <h4>14000+</h4>
+                  <h5>Newsletter Subscriber</h5>
+               </div>
+               <div class="numdata">
+                  <h4>60</h4>
+                  <h5>Average Job Applications</h5>
+               </div>
+               <div class="numdata">
+                  <h4>20</h4>
+                  <h5>Average Days to Hỉre</h5>
+               </div>
+               <div class="buttonpost">
+                  <button>Post a Job</button>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="remotejobs">
+         <h3>Remote jobs by category</h3>
+         <h5>Explore Category</h5>
+         <div class="remotejob">
+            <div class="whitebgr">
+               <div class="major">
+                  <img src="/Image/2.jpeg" alt="">
+                  <h4>Business Development</h4>
+                  <h6>649 Job Vacancy</h6>
+               </div>
+            </div>
+            <div class="whitebgr">
+               <div class="major">
+                  <img src="/Image/3.jpeg" alt="">
+                  <h4>Design & Development</h4>
+                  <h6>341 Job Vacancy</h6>
+               </div>
+            </div>
+            <div class="whitebgr">
+               <div class="major">
+                  <img src="/Image/4.jpeg" alt="">
+                  <h4>Copywriting & Content Writing</h4>
+                  <h6>216 Job Vacancy</h6>
+               </div>
+            </div>
+            <div class="whitebgr">
+               <div class="major">
+                  <img src="/Image/5.jpeg" alt="">
+                  <h4>Marketing & Communication </h4>
+                  <h6>543 Job Vacancy</h6>
+               </div>
+            </div>
+         
+         
+         </div>
+      </div>
+      
+   </div>
+   <div class="pagethree">
+      <div class="imageleft">
+         <div class="purplecircle">
+            <img src="/Image/10.png" alt="">
+         </div>
+
+      </div>
+      <div class="boostyourcareer">
+         <h3>Boost your career <br>by choosing Remotely<br></h3>
+         <h6>But I must explain to you how all this mistaken 
+            idea of denouncing pleaseure and praising pain 
+            was born and I will give you a complete account 
+            of the system. </h6>
+         <div class="check">
+            <div class="must">
+               <i class="fas fa-check-circle"></i>
+               <h5>I must explain to you how all this mistaken idea</h5>
+            </div>
+            <div class="must">
+               <i class="fas fa-check-circle"></i>
+               <h5>I must explain to you how all this mistaken idea of denouncing</h5>
+            </div>
+            <div class="must">
+               <i class="fas fa-check-circle"></i>
+               <h5>I must explain to you how all this mistaken idea</h5>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="pagefour">
+      
+      <div class="boostyourcareer whatourjob">
+         <h3>What our job <br>seekers are saying<br></h3>
+         <div><i class="fas fa-quote-left"></i></div>
+         <h6>But I must explain to you how all this mistaken
+            idea of denouncing pleaseure and praising pain
+            was born and I will give you a complete account
+            of the system. </h6>
+         <h5>Benjamin Fuller</h5>
+         <h6 id="noitalic">UX Designer at Google</h6>
+         <div class="arrowsquare">
+            <div class="square">
+               <i class="fas fa-arrow-left"></i>
+            </div>
+            <div class="square">
+               <i class="fas fa-arrow-right"></i>
+            </div>
+
+         </div>
+         
+      </div>
+      <div class="imageright">
+         <div class="">
+            <img src="/Image/6.png" alt="">
+         </div>
+      
+      </div>
+   </div>
+   <div class="pagefive">
+      <div class="subscribebox">
+         <div class="boxpurple">
+            <h4 id="subscribe">Subscribe to our newsletter</h4>
+            <h5>News update, notifications, job alerts for job seekers & employers</h5>
+            <div class="submail">
+               <form action=""><input type="text" placeholder="Type email address"><button><i class="fas fa-paper-plane"></i></button></form>
+            </div>
+         </div>
+      </div>
+   </div>
+</body>
+<footer>
+   <div class="footerremote">
+      <div class="remotelyfoot">
+         <h3>Remotely</h3>
+         <h6>But I must explain to you how all this <br> mistaken idea
+            of denouncing pleasure and praising pain was born.</h6>
+         <div class="socialicons">
+            <div class="socialicon">
+               <i class="fab fa-facebook-f faa"></i>
+            </div>
+            
+            <i class="fab fa-twitter faa"></i>
+            <i class="fab fa-instagram faa"></i>
+            <i class="fab fa-youtube faa"></i>
+         </div>
+         <p>&copy 2021 Company. All Rights Reserved. Moon Company </p>
+      </div>
+      <div class="categories">
+         <h3>Categories</h3>
+         <h6>ios Developers</h6>
+         <h6>Front-End Developers</h6>
+         <h6>UX Designers</h6>
+         <h6>UI Designers</h6>
+         <h6>Writing Transtation</h6>
+         <h6>Program & Tech</h6>
+      </div>
+      <div class="community">
+         <h3>Community</h3>
+         <h6>Events</h6>
+         <h6>Blog</h6>
+         <h6>Forum</h6>
+         <h6>Podcast</h6>
+         <h6>Affiliates</h6>
+         <h6>Invite a Friend</h6>
+      </div>
+      <div class="About">
+         <h3>About</h3>
+         <h6>About Us</h6>
+         <h6>Partnerships</h6>
+         <h6>Finance experts</h6>
+         <h6>Project Management</h6>
+         <h6>Project Manager</h6>
+         <h6>The Team</h6>
+      </div>
+      <div class="Contact">
+         <h3>Contact</h3>
+         <h6>Contact Us</h6>
+         <h6>Press Center</h6>
+         <h6>Careers</h6>
+         <h6>Faq</h6>
+      </div>
+   </div>
+</footer>
+</html>
